67617f96b4183354841e2043fb9162fe0b630572
youzuo <1193108742@qq.com>
2019-01-18 10:44:37 +0800

    Emoji

--- app/src/main/java/com/youzuo/designbyyz/MainActivity.java
+++ app/src/main/java/com/youzuo/designbyyz/MainActivity.java
@@ -1,22 +1,73 @@
 package com.youzuo.designbyyz;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
+import com.luolc.emojirain.EmojiRainLayout;
 import com.youzuo.library.Base.BaseActivity;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class MainActivity extends BaseActivity {
+
+    @BindView(R.id.group_emoji_container)
+    EmojiRainLayout groupEmojiContainer;
+    @BindView(R.id.mButton)
+    Button mButton;
+    @BindView(R.id.mButtonStop)
+    Button mButtonStop;
+    @BindView(R.id.mButtons)
+    Button mButtons;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setSetStatusBar(true,true);
+        ButterKnife.bind(this);
+        setStatusBarFontColot(true, true);
+// add emoji sources
+        groupEmojiContainer.addEmoji(R.drawable.emoji_1_3);
+        groupEmojiContainer.addEmoji(R.drawable.emoji_2_3);
+        groupEmojiContainer.addEmoji(R.drawable.emoji_3_3);
+        groupEmojiContainer.addEmoji(R.drawable.emoji_4_3);
+        groupEmojiContainer.addEmoji(R.drawable.emoji_5_3);
+
+        // set emojis per flow, default 6
+        groupEmojiContainer.setPer(10);
+
+        // set total duration in milliseconds, default 8000
+        groupEmojiContainer.setDuration(7200);
+
+        // set average drop duration in milliseconds, default 2400
+        groupEmojiContainer.setDropDuration(2400);
+
+        // set drop frequency in milliseconds, default 500
+        groupEmojiContainer.setDropFrequency(500);
     }
 
     @Override
     public void widgetClick(View view) {
 
     }
+
+    @OnClick({R.id.mButton, R.id.mButtonStop, R.id.mButtons})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.mButton:
+                groupEmojiContainer.startDropping();
+                break;
+            case R.id.mButtonStop:
+                groupEmojiContainer.stopDropping();
+                break;
+            case R.id.mButtons:
+                Intent intent = new Intent(MainActivity.this,EmojiActivity.class);
+                startActivity(intent);
+                break;
+        }
+    }
+
 }

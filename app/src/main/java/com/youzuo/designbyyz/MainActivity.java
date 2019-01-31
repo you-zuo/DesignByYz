138ca3bcc7ec0154d802351d8c46367a288f1822
youzuo <1193108742@qq.com>
2019-01-31 17:51:39 +0800

    Emoji

--- app/src/main/java/com/youzuo/designbyyz/MainActivity.java
+++ app/src/main/java/com/youzuo/designbyyz/MainActivity.java
@@ -7,6 +7,7 @@ import android.widget.Button;
 
 import com.luolc.emojirain.EmojiRainLayout;
 import com.youzuo.library.Base.BaseActivity;
+import com.youzuo.library.Utils.PhoneUtil;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -22,6 +23,8 @@ public class MainActivity extends BaseActivity {
     Button mButtonStop;
     @BindView(R.id.mButtons)
     Button mButtons;
+    @BindView(R.id.mShortMessage)
+    Button mShortMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +34,7 @@ public class MainActivity extends BaseActivity {
         setStatusBarFontColot(true, true);
 // add emoji sources
         groupEmojiContainer.addEmoji(R.drawable.emoji_1_3);
-        groupEmojiContainer.addEmoji(R.drawable.emoji_2_3);
+        groupEmojiContainer.addEmoji(R.mipmap.ic_launcher);
         groupEmojiContainer.addEmoji(R.drawable.emoji_3_3);
         groupEmojiContainer.addEmoji(R.drawable.emoji_4_3);
         groupEmojiContainer.addEmoji(R.drawable.emoji_5_3);
@@ -51,10 +54,10 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void widgetClick(View view) {
-
+        System.out.println();
     }
 
-    @OnClick({R.id.mButton, R.id.mButtonStop, R.id.mButtons})
+    @OnClick({R.id.mButton, R.id.mButtonStop, R.id.mButtons,R.id.mShortMessage})
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.mButton:
@@ -64,8 +67,11 @@ public class MainActivity extends BaseActivity {
                 groupEmojiContainer.stopDropping();
                 break;
             case R.id.mButtons:
-                Intent intent = new Intent(MainActivity.this,EmojiActivity.class);
-                startActivity(intent);
+//                Intent intent = new Intent(MainActivity.this,. class);
+//                startActivity(intent);
+                break;
+            case R.id.mShortMessage:
+                PhoneUtil.sendMessage(MainActivity.this, "111111111111", "你好啊");
                 break;
         }
     }
